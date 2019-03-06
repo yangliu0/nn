192662704eb987b42a9651688070de4c1f0ea7b5
yangliu0 <19414123@qq.com>
2019-03-06 19:30:48 +0800

    新增前馈神经网络

---
+++ pytorch_learning/basic/feedforward_neural_network/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+# @Time 2019/3/6
+# @Author liuyang
---
+++ pytorch_learning/basic/feedforward_neural_network/main.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+input_size = 784
+hidden_size = 500
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# Data Loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+
+# Fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+# Model
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, 28 * 28).to(device)
+        labels = labels.to(device)
+
+        # Forward propagation
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # BackWard propagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:4f}'.format(epoch + 1, num_epochs, i + 1, total_step,
+                                                                    loss.item()))
+
+# Test the model
+with torch.no_grad():
+    corret = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28 * 28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, dim=1)
+        total += labels.size(0)
+        corret += (predicted == labels).sum().item()
+
+    print('Accuracy: {} %'.format(100 * corret / total))
